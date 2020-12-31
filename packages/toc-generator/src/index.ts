dfa90c073defa0979fff9c362517a70f6ad62bae
Krzysztof Żuraw <services@kzuraw.com>
2020-12-31 12:37:40 +0100

    feat: read md files from folder

--- packages/toc-generator/src/index.ts
+++ packages/toc-generator/src/index.ts
@@ -1,29 +1,45 @@
-import {Command, flags} from '@oclif/command'
+/* eslint-disable @typescript-eslint/ban-ts-ignore */
+import { Command, flags } from "@oclif/command";
+import { glob } from "glob";
+import * as path from "path";
 
 class TocGenerator extends Command {
-  static description = 'describe the command here'
+  static description = "describe the command here";
 
   static flags = {
-    // add --version flag to show CLI version
-    version: flags.version({char: 'v'}),
-    help: flags.help({char: 'h'}),
+    version: flags.version({ char: "v" }),
+    help: flags.help({ char: "h" }),
+    inputFolder: flags.string({
+      char: "f",
+      default: ".",
+      required: true,
+      description: "path to input folder",
+    }),
     // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
+    // name: flags.string({ char: "n", description: "name to print" }),
     // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
-  }
+    // force: flags.boolean({ char: "f" }),
+  };
 
-  static args = [{name: 'file'}]
+  static args = [{ name: "file" }];
 
   async run() {
-    const {args, flags} = this.parse(TocGenerator)
+    const { args, flags } = this.parse(TocGenerator);
 
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from ./src/index.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
+    glob(
+      "**/*.md",
+      { cwd: path.resolve(flags.inputFolder), ignore: "README.md" },
+      (err, matches) => {
+        if (err) {
+          this.log(err.message ?? "error");
+        }
+        matches.forEach((match) => this.log(match));
+      }
+    );
+    // if (args.file && flags.force) {
+    //   this.log(`you input --force and --file: ${args.file}`);
+    // }
   }
 }
 
-export = TocGenerator
+export = TocGenerator;
